e86aa79956d099b5b1d03d762c8ed00a635fd812
Chenowoo <cjsgusdn14@naver.com>
2023-12-09 18:40:09 +0900

    feat(PairInitializing): 페어 매칭을 초기화하는 기능 구현

--- src/main/java/pairmatching/controller/PairInitializingController.java
+++ src/main/java/pairmatching/controller/PairInitializingController.java
@@ -1,9 +1,12 @@
 package pairmatching.controller;
 
+import pairmatching.model.PairMatchResult;
+
 public class PairInitializingController implements Controller{
 
+    PairMatchResult pairMatchResult = PairMatchResult.getInstance();
     @Override
     public void process() {
-
+        pairMatchResult.initPairMatch();
     }
 }
