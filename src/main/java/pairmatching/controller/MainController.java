23dc51d86416a5e8113f038bd65611d708aa2d7d
Chenowoo <cjsgusdn14@naver.com>
2023-12-09 13:10:53 +0900

    feat(MainController): 페어 매칭을 관리하는 기능 구현

---
+++ src/main/java/pairmatching/controller/MainController.java
@@ -0,0 +1,39 @@
+package pairmatching.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import pairmatching.constant.SystemMessage;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public class MainController {
+
+    public static final String END = "Q";
+    private final Map<String, Controller> controllers = new HashMap<>();
+
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public MainController() {
+        controllers.put("1", new PairMatchingController());
+        controllers.put("2", new PairRetrievingController());
+        controllers.put("3", new PairInitializingController());
+        controllers.put("4", new PairEndController());
+    }
+
+    public void run() {
+        MainController mainController = new MainController();
+
+        String option = "0";
+        while (!option.equals(END)) {
+            outputView.print(SystemMessage.SELECT_OPTION);
+            option = inputView.inputOption();
+            mainController.service(option);
+        }
+    }
+
+    public void service(String option) {
+        Controller controller = controllers.get(option);
+        controller.process();
+    }
+}
