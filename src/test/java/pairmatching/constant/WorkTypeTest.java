69206c1dba223e240a7cadd1c734984f1684f3c1
endb1228 <85601916+endb1228@users.noreply.github.com>
2023-12-09 11:47:01 +0900

    Test: 작업 선택 입력에 대한 테스트 구현

--- src/test/java/pairmatching/constant/WorkTypeTest.java
+++ src/test/java/pairmatching/constant/WorkTypeTest.java
@@ -1,13 +1,22 @@
 package pairmatching.constant;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class WorkTypeTest {
 
     @Test
-    void get() {
-        // TODO
+    void 실패_잘못된_값을_입력했을_경우() {
+        Assertions.assertThatThrownBy(() -> WorkType.get("4"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "Q"})
+    void 성공_적절한_값을_입력했을_경우(String input) {
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> WorkType.get(input));
     }
 }
