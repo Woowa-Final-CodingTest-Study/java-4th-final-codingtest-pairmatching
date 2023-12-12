b0defe1f440000ea88fcd9786ddbc5360f9e9dc4
YoonseolLee <thisyoon97@gmail.com>
2023-12-12 11:32:20 +0900

    feat: MatchingHistory, MatchingHistoryRepository 구현

---
+++ src/main/java/pairmatching/domain/MatchingHistory.java
@@ -0,0 +1,22 @@
+package pairmatching.domain;
+
+import java.util.List;
+import pairmatching.service.MatchingService;
+
+public class MatchingHistory {
+    private Course course;
+    private List<Pair> pair;
+
+    public MatchingHistory(Course course, List<Pair> pair) {
+        this.course = course;
+        this.pair = pair;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public List<Pair> getPair() {
+        return pair;
+    }
+}
